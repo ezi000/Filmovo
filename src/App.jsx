466340e2ed058a2c1a9659e4985d5487ea394242
ezi000 <ezio18@o2.pl>
2024-03-04 20:34:13 +0100

    Home

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="logo">Filmovo</h1>
+      <div className="buttons">
+        <button>Log in</button>
+        <button>Sign in</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
