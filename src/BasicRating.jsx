7f170eca71aaa49d39f7577e94586606361d1207
ezi000 <ezio18@o2.pl>
2024-05-22 09:57:52 +0200

    .

--- src/BasicRating.jsx
+++ src/BasicRating.jsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
 
 export default function BasicRating() {
   const [value, setValue] = React.useState(2);
@@ -9,7 +9,7 @@ export default function BasicRating() {
   return (
     <Box
       sx={{
-        '& > legend': { mt: 2 },
+        "& > legend": { mt: 2 },
       }}
     >
       <Typography component="legend">Rate The Movie</Typography>
@@ -20,8 +20,6 @@ export default function BasicRating() {
           setValue(newValue);
         }}
       />
-     
-     
     </Box>
   );
 }
